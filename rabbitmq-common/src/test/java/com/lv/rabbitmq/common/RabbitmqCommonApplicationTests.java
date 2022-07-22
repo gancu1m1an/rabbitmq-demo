507be143fe87ec17c713cf35c9e8d938f2e8ef3b
gancu1m1an <62418479+gancu1m1an@users.noreply.github.com>
2022-07-22 16:18:16 +0800

    Add files via upload

---
+++ rabbitmq-common/src/test/java/com/lv/rabbitmq/common/RabbitmqCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv.rabbitmq.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
